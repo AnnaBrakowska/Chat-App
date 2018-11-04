2db2f893d58a440a0498ae37ac77acd1e19ddf17
Anna <anna.brakowska91@gmail.com>
2018-11-04 11:34:40 -0500

    changes to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 /*** Serving static files***/
 const path = require("path");
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
+app.listen(PORT, function() {
+  console.log("Listening to port" + PORT);
 });
